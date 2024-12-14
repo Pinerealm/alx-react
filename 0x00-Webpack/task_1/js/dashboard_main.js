482a03c173b72a9c8d97bcc22aa595c0c104bbd3
Pinerealm <osalami09@gmail.com>
2024-12-14 13:51:34 +0300

    Fix quote style for count element in dashboard_main.js

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -5,7 +5,7 @@ $(function() {
   $('body').append('<p>ALX Dashboard</p>');
   $('body').append('<p>Dashboard data for the students</p>');
   $('body').append('<button>Click here to get started</button>');
-  $('body').append("<p id='count'></p>");
+  $('body').append('<p id=\'count\'></p>');
   $('body').append('<p>Copyright - ALX</p>');
 
   let count = 0;
