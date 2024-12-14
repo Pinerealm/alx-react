52a6d01d6634854de3de9e2903ae8d05c49fded8
Pinerealm <osalami09@gmail.com>
2024-12-14 11:41:25 +0300

    Minor changes

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,7 +1,7 @@
-import $ from "jquery";
+import $ from 'jquery';
 
 $(function() {
-  $("body").append("<p>ALX Dashboard</p>");
-  $("body").append("<p>Dashboard data for the students</p>");
-  $("body").append("<p>Copyright - ALX</p>");
+  $('body').append('<p>ALX Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<p>Copyright - ALX</p>');
 });
