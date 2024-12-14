fba2180bec16a3f3124bfe6e6874c550b29b8ebc
Pinerealm <osalami09@gmail.com>
2024-12-14 13:09:28 +0300

    Use Webpack with a config file

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,17 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+$(function() {
+  $('body').append('<p>ALX Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button>Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - ALX</p>');
+
+  const updateCounter = () => {
+    let count = parseInt($('#count').text()) || 0;
+    $('#count').text(`${count + 1} clicks on the button`);
+  };
+
+  $('button').on('click', _.debounce(updateCounter, 1000));
+});
