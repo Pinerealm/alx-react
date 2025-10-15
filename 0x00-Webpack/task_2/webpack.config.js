fb1afa65450bcdc801c18198912e7fcd12acf344
Pinerealm <osalami09@gmail.com>
2025-10-16 01:27:02 +0300

    Task 2: Initialize project with Webpack setup, CSS styles, and basic HTML structure

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    main: path.resolve(__dirname, "./js/dashboard_main.js"),
+  },
+  performance: {
+    maxAssetSize: 512000, // 500 KiB
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
